a6f080537ab8487e0cb5686278200727f1a63ff4
mohamadgazer <mohamadgazer@yahoo.com>
2023-12-27 18:33:14 +0200

    fix merge

--- lib/main.dart
+++ lib/main.dart
@@ -5,28 +5,3 @@ import 'main_app.dart';
 void main() {
   runApp(const MainApp());
 }
-<<<<<<< HEAD
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              Text('Hello World! after 5th test'),
-              Text('Hello World! after 5th test'),
-              Text('Hello World! after 5th test'),
-              Text('Hello World! after 5th test'),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-=======
->>>>>>> prepare_material_app

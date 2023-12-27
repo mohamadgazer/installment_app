cbfd6d5b314673958d98b886b5142d39e995827d
mohamadgazer <mohamadgazer@yahoo.com>
2023-12-27 18:39:03 +0200

    6th test

--- lib/features/homePage/widget/home_page_body.dart
+++ lib/features/homePage/widget/home_page_body.dart
@@ -8,7 +8,7 @@ class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('forth test'),
+      child: Text('6th test'),
     );
   }
 }

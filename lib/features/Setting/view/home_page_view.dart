64eb284e44501fddd406e0cef2e9f7ad1e0a6399
mohamadgazer <mohamadgazer@yahoo.com>
2023-12-28 13:59:34 +0200

    set setting page view

---
+++ lib/features/Setting/view/home_page_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../widget/home_page_body.dart';
+
+class SettingPageView extends StatelessWidget {
+  static String id = "/settingid";
+  const SettingPageView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: HomePageBody(),
+    );
+  }
+}
---
+++ lib/features/Setting/widget/home_page_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:installment/generated/l10n.dart';
+
+class HomePageBody extends StatelessWidget {
+  const HomePageBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(S.of(context).save_data),
+    );
+  }
+}

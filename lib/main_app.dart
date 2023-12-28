1e46bd4e8ea21ade996e6ac9390c95f1f7010cfb
mohamadgazer <mohamadgazer@yahoo.com>
2023-12-28 14:32:39 +0200

    add ScreenUtilInit in main app

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:installment/core/app_local.dart';
 import 'package:installment/core/routing/app_routers.dart';
 import 'package:installment/generated/l10n.dart';
-
-import 'features/homePage/view/home_page_view.dart';
 
 class MainApp extends StatelessWidget {
   final AppRouter appRouter;
@@ -11,12 +10,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: localizationsDelegates,
-      supportedLocales: S.delegate.supportedLocales,
-      locale: const Locale("ar"),
-      initialRoute: HomePageView.id,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: localizationsDelegates,
+        supportedLocales: S.delegate.supportedLocales,
+        locale: const Locale("ar"),
+        onGenerateRoute: appRouter.generateRoute,
+      ),
     );
   }
 }

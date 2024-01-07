5448d019d8317b5d97a9030f0417a9a91f0487c5
mohamadgazer <mohamadgazer@yahoo.com>
2024-01-08 01:14:35 +0200

    test context : it work ✅

--- lib/features/homePage/view/home_page_view.dart
+++ lib/features/homePage/view/home_page_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:installment/core/helper/context_helper.dart';
+import 'package:installment/features/Setting/view/setting_view.dart';
 
 import '../widget/home_page_body.dart';
 
@@ -13,9 +15,11 @@ class HomePageView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          IconButton(onPressed: () {
-            Naviga
-          }, icon: const Icon(Icons.settings))
+          IconButton(
+              onPressed: () {
+                context.pushNamed(SettingPageView.id);
+              },
+              icon: const Icon(Icons.settings))
         ],
       ),
       body: const HomePageBody(),

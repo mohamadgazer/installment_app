d1b131a9a1935413dd8efbf53ae8442b36f4918d
mohamadgazer <mohamadgazer@yahoo.com>
2024-01-08 02:52:49 +0200

    set controller to pageview

--- lib/features/onboarding/widgt/on_boarding_body.dart
+++ lib/features/onboarding/widgt/on_boarding_body.dart
@@ -3,14 +3,32 @@ import 'package:installment/features/onboarding/data/onboarding_local_data.dart'
 
 import 'on_boarding_page_view_item.dart';
 
-class OnBoardingBody extends StatelessWidget {
+class OnBoardingBody extends StatefulWidget {
   const OnBoardingBody({
     super.key,
   });
 
   @override
+  State<OnBoardingBody> createState() => _OnBoardingBodyState();
+}
+
+class _OnBoardingBodyState extends State<OnBoardingBody> {
+  PageController controller = PageController(initialPage: 0);
+
+  @override
+  void initState() {
+    controller.initialPage;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(child: PageView.builder(
+    return SafeArea(
+        child: PageView.builder(
+      controller: controller,
+      onPageChanged: (value) {
+        if (value > onboardingData(context).length) {}
+      },
       itemBuilder: (context, index) {
         return OnBoardingPageViewItem(
           list: onboardingData(context),
